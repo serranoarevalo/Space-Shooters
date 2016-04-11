2bcb7f17fc646d9bbafede6280f876d12d463c88
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-11 10:28:07 +0700

    Added game controller with randomly instantiated asteroids

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+
+	void Start(){
+
+		SpanWaves ();
+	
+	}
+
+	void SpanWaves()
+	{
+		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazard, spawnPosition, spawnRotation);
+	}
+
+}
